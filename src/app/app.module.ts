8208d724b5aa269df938b5bb1ae4430e01096e7d
Federico Ferrante <federico.ferrante@hotmail.it>
2023-03-20 15:12:08 +0100

    Prima Creazione
    Descrizione: primo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimaPaginaComponent } from './prima-pagina/prima-pagina.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimaPaginaComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/prima-pagina/prima-pagina.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prima-pagina',
+  templateUrl: './prima-pagina.component.html',
+  styleUrls: ['./prima-pagina.component.scss']
+})
+export class PrimaPaginaComponent {
+
+}
